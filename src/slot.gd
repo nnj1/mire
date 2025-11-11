5c77935cf219055504ff605aa8716b247b3c5fb0
Newlyn Joseph <new.joseph10@gmail.com>
2025-11-11 18:10:20 -0500

    switched to compatability renderer

--- src/slot.gd
+++ src/slot.gd
@@ -1,12 +1,12 @@
 extends ColorRect
 
 # the JSON document containing item data
-var item = null
+var item: Dictionary
 
-func prepare(item) -> void:
-	self.item = item
-	self.tooltip_text = item.name
-	get_node('button').texture = load(item.icon)
+func prepare(item_given) -> void:
+	self.item = item_given
+	get_node('button').tooltip_text = item.name
+	get_node('button').icon = load(item.icon)
 	if item.active:
 		self.color = Color(0.573,0.573,0.573, 0.63)
 	
@@ -18,6 +18,5 @@ func _ready() -> void:
 	
 	pass
 
-func _on_button_gui_input(event: InputEvent) -> void:
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		get_tree().get_root().get_node('Node2D').show_item_view(self.item)
+func _on_button_pressed() -> void:
+	get_tree().get_root().get_node('Node2D').show_item_view(self.item)
